4e1f3db18a44b1446b4c3e3359d9c48037630cac
AndreaBasile97 <andreabasile97@gmail.com>
2024-06-26 12:01:20 +0200

    Integration completed with med-ddpm

--- src/visualization/visualize_scans.py
+++ src/visualization/visualize_scans.py
@@ -1,12 +1,32 @@
 from nilearn import plotting, image
+import argparse
 
 
-def show_image(image_path):
+def show_image(image_path, cut_coords):
     img = image.load_img(image_path)
-    plotting.plot_img(img)
+    plotting.plot_img(
+        img, cut_coords=cut_coords, display_mode="ortho", title="NIFTI Image"
+    )
     plotting.show()
 
 
+def main():
+    parser = argparse.ArgumentParser(description="Visualize a .nii.gz image.")
+    parser.add_argument(
+        "--image_path", type=str, required=True, help="Path to the .nii.gz image file."
+    )
+    parser.add_argument(
+        "--cut_coords",
+        type=float,
+        nargs=3,
+        required=True,
+        help="The x, y, and z coordinates of the slice to display.",
+    )
+    args = parser.parse_args()
+
+    # Plot the image using nilearn's plotting function
+    show_image(args.image_path, args.cut_coords)
+
+
 if __name__ == "__main__":
-    image_path = "data/raw/HC_T1/NIFD_1_S_0071_MR_T1_mprage__br_raw_20120819160257384_19_S161460_I324709.nii"
-    show_image(image_path)
+    main()
